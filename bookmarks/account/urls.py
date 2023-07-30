5178cade9015cb79bd2fe4ad3c12dd3bec1520ce
SashDu <sashadubina1996@gmail.com>
2023-07-31 02:10:49 +0300

    added some configurations for created profiles of users

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 
+
 urlpatterns = [
     #path('login/', views.user_login, name='login' ),
 
@@ -36,6 +37,8 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
 
     path('', views.dashboard, name='dashboard'),
+
+    path('register/', views.register, name='register'),
 
 
 ]
